3167c30963d9b25f7cf5a432cf248ea8b278eab7
Mike Donnici <michael@mesa.net.au>
2017-10-11 09:07:50 +1100

    Initial

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="app">
+
+    </div>
+</template>
+
+<script>
+  export default {
+
+    data() {
+      return {
+        galleries: []
+      }
+    },
+
+    methods: {
+      fetchImages() {
+        this.$http.get('/static/images.json')
+          .then((r) => {
+            console.log(r)
+          }, (r) => {
+            console.log('Could not fetch images.json')
+          })
+      }
+    },
+
+    mounted() {
+      this.fetchImages()
+    }
+  }
+</script>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex-flow: row wrap;
+        background-color: #fff;
+        color: #444;
+    }
+
+    .box {
+        margin: 5px;
+        background-color: #444;
+        color: #fff;
+        border-radius: 5px;
+        padding: 20px;
+        font-size: 150%;
+    }
+</style>
